830befaaa4f598239bc9c0328d181fcd8d0593c8
alexander.beletsky <alexander.beletsky@gmail.com>
2012-11-07 15:24:57 +0200

    added scenarios for getting computed fields

--- src/backbone.computedfields.js
+++ src/backbone.computedfields.js
@@ -4,13 +4,55 @@
         throw 'Please include Backbone.js before Backbone.ComputedFields.js';
     }
 
-    var ComputedFields = function () {
+    var ComputedFields = function (model) {
+        this.model = model;
+        this._computedFields = [];
 
+        this.initialize();
     };
 
     ComputedFields.VERSION = '0.0.1';
 
     _.extend(ComputedFields.prototype, {
+        initialize: function () {
+            _.bindAll(this);
+
+            this._lookUpComputedFields();
+            this._decorateModel();
+        },
+
+        _lookUpComputedFields: function () {
+            for (var obj in this.model) {
+                var field = this.model[obj];
+
+                if (field && (field.set || field.get)) {
+                    this._computedFields.push({name: obj, field: field});
+                }
+            }
+        },
+
+        _decorateModel: function () {
+            this._originalGet = this.model.get;
+            this.model.get = this._get;
+        },
+
+        _get: function (attr) {
+            var computedField = this.computedField(attr);
+            if (computedField && computedField.get) {
+                return computedField.get.apply(this.model, arguments);
+            }
+
+            return this._originalGet.apply(this.model, arguments);
+        },
+
+        computedField: function (attr) {
+            var computedField = _.find(this._computedFields, function (cf) {
+                return cf.name === attr;
+            });
+
+            return computedField && computedField.field;
+        }
+
 
     });
 
--- test/spec/computedfields.spec.js
+++ test/spec/computedfields.spec.js
@@ -1,17 +1,123 @@
 describe('Backbone.ComputedFields spec', function() {
 
-    describe('when Backbone.ComputedFields is constructing', function () {
+    describe('when ComputedFields initialized in Backbone.Model', function () {
 
-        var computedFields;
+        var model;
 
         beforeEach(function () {
-            computedFields = new Backbone.ComputedFields();
+            var Model = Backbone.Model.extend({
+                initialize: function () {
+                    this.computedFields = new Backbone.ComputedFields(this);
+                },
+
+                grossPrice: {
+                    get: function () {
+                        return 100;
+                    }
+                }
+            });
+
+            model = new Model({ netPrice: 100, vatRate: 5});
         });
 
-        it ('should exist', function () {
-            expect(computedFields).to.exist;
+        it ('should be initialized', function () {
+            expect(model.computedFields).to.exist;
+            expect(model.computedFields._computedFields.length).to.equal(1);
+        });
+
+        it ('should access model attributes', function () {
+            expect(model.get('netPrice')).to.equal(100);
+            expect(model.get('vatRate')).to.equal(5);
         });
 
     });
+
+    describe('when ComputedFields are used', function () {
+        beforeEach(function () {
+            var Model = Backbone.Model.extend({
+                defaults: {
+                    'netPrice': 0.0,
+                    'vatRate': 0.0
+                },
+
+                initialize: function () {
+                    this.computedFields = new Backbone.ComputedFields(this);
+                },
+
+                grossPrice: {
+                    get: function () {
+                        return 105;
+                    }
+                }
+            });
+
+            model = new Model({ netPrice: 100, vatRate: 5});
+        });
+
+        it ('should calculate grossPrice', function () {
+            expect(model.get('grossPrice')).to.equal(105);
+        });
+
+    });
+
+
+    // describe('when Backbone.Model is extened with ComputedFields', function () {
+
+    //     var model;
+
+    //     beforeEach(function () {
+    //         var Model = Backbone.ComputedFields({
+    //             defaults: {
+    //                 'netPrice': 0.0
+    //             }
+    //         });
+
+    //         model = new Model();
+    //     });
+
+    //     it ('should plain attributes be accessible as before', function () {
+    //         expect(model.get('netPrice')).to.equal(0.0);
+    //     });
+
+    //     it ('should give undefined for attributes which does not exist', function () {
+    //         expect(model.get('some')).to.not.exist;
+    //     });
+
+
+        // describe('when computed fields are used', function () {
+
+        //     describe('getting values from computed fields', function () {
+
+        //         beforeEach(function () {
+
+        //             var Model = Backbone.ComputedFields(Backbone.Model, {
+        //                 defaults: {
+        //                     'netPrice': 0.0,
+        //                     'vatRate': 5
+        //                 },
+
+        //                 initialize: function () {
+        //                     this.computedField(this.grossPrice);
+        //                 },
+
+        //                 grossPrice: {
+        //                     depends: ['netPrice', 'vatRate'],
+        //                     get: function (depends) {
+        //                         return depends.netPrice * (1 + 1 / depends.vatRate);
+        //                     }
+        //                 }
+        //             });
+
+        //             model = new Model({ netPrice: 100 });
+        //         });
+
+        //         it ('should get value of computed field', function () {
+        //             expect(model.get('grossPrice')).to.equal(105);
+        //         });
+        //     });
+
+        // });
+
+    // });
 
 });
